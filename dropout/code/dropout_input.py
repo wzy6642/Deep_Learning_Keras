33fed1ebf6da44654f71798887845d9e230f322d
zhenyu wu <33506993+wzy6642@users.noreply.github.com>
2019-01-10 20:43:47 +0800

    Add files via upload

---
+++ dropout/code/dropout_input.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 20:28:52 2019
+
+@author: wzy
+"""
+from sklearn import datasets
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.optimizers import SGD
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+
+
+dataset = datasets.load_iris()
+x = dataset.data
+Y = dataset.target
+seed = 4
+np.random.seed(seed)
+def create_model(init='glorot_uniform'):
+    model = Sequential()
+    # 每个更新周期中20%的输入将被随机排除
+    model.add(Dropout(rate=0.2, input_shape=(4,)))
+    model.add(Dense(units=4, activation='relu', kernel_initializer=init))
+    model.add(Dense(units=6, activation='relu', kernel_initializer=init))
+    model.add(Dense(units=3, activation='softmax', kernel_initializer=init))
+    # 定义优化器
+    sgd = SGD(lr=0.01, momentum=0.8, decay=0.0, nesterov=False)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
+
+model = KerasClassifier(build_fn=create_model, epochs=200, batch_size=5, verbose=1)
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(model, x, Y, cv=kfold)
+print('Accuracy: %.2f%% (%.2f)' % (results.mean()*100, results.std()))
+
