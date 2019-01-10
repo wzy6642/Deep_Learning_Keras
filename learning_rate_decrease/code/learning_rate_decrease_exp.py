a6a3884e0a3e0ecf2138d5b615c4dc27498d9846
zhenyu wu <33506993+wzy6642@users.noreply.github.com>
2019-01-10 21:55:04 +0800

    Add files via upload

---
+++ learning_rate_decrease/code/learning_rate_decrease_exp.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 21:44:00 2019
+
+@author: wzy
+"""
+from sklearn import datasets
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.callbacks import LearningRateScheduler
+from math import pow, floor
+
+
+dataset = datasets.load_iris()
+x = dataset.data
+Y = dataset.target
+seed = 4
+np.random.seed(seed)
+
+
+# 计算学习率
+def step_decay(epoch):
+    init_lrate = 0.01
+    drop = 0.05
+    epochs_drop = 10
+    lrate = init_lrate * pow(drop, floor(1+epoch) / epochs_drop)
+    return lrate
+
+
+def create_model(init='glorot_uniform'):
+    model = Sequential()
+    model.add(Dense(units=4, activation='relu', input_dim=4, kernel_initializer=init))
+    model.add(Dense(units=6, activation='relu', kernel_initializer=init))
+    model.add(Dense(units=3, activation='softmax', kernel_initializer=init))
+    # 定义优化器
+    learningRate = 0.01
+    momentum = 0.9      # 动量参数
+    decay_rate = 0.0    # 学习率衰减
+    sgd = SGD(lr=learningRate, momentum=momentum, decay=decay_rate, nesterov=False)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
+
+lrate = LearningRateScheduler(step_decay)
+model = KerasClassifier(build_fn=create_model, epochs=200, batch_size=5, verbose=1, callbacks=[lrate])
+model.fit(x, Y)
+
